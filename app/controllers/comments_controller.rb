8dada121f2626e71f539902e6a882f00e2e5edd1
Hiro-yoshi <161566433+Hiro-yoshi@users.noreply.github.com>
2024-05-15 14:53:29 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,16 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    @prototype = Prototype.find(params[:prototype_id])
+    if @comment.save
+      redirect_to "/prototypes/#{@comment.prototype_id}"
+    else
+      render "/prototypes/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+end
